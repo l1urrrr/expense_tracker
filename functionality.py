a60b6ac24f13093f536eb94a87ebe927c9fe49d6
l1urrrr <loliaamabibi@gmail.com>
2025-12-30 23:41:57 +0000

    add method to calculate average and conceptualising the method to show categorical percentages of the expenses dynamically

--- functionality.py
+++ functionality.py
@@ -1,4 +1,5 @@
 from expense import Expense
+#
 
 def show_budget():
     inp_budget = float(input("Enter budget: "))
@@ -24,7 +25,8 @@ def sum_expenses():
         chunks  = file.read().split("||") # read each line and separate into
         # groups
         for y in chunks: #loop through each chunk
-            if not chunks.strip():
+            if not chunks.strip(): #checks if it is the beginning or end of
+                # the file
                 continue
             lines = y.split("\n")
             for line in lines:
@@ -35,14 +37,39 @@ def sum_expenses():
 
 def rem_budget():
     global budget
-    sum = sum_expenses()
-    new_budget = float(budget-sum)
+    tot = sum_expenses()
+    new_budget = float(budget-tot)
     return new_budget
 
+def avg_spent():
+    expenses = 0
+    with open("expenses.txt", "r") as file:  # open file and close it when done
+        chunks = file.read().split("||")  # read each line and separate into
+        # groups
+        for y in chunks:  # loop through each chunk
+            if not chunks.strip():  # checks if it is the beginning or end of
+                # the file
+                continue
+            lines = y.split("\n")
+            for line in lines:
+                if line.startswith("amount") and float(line.split(":")[1]) > 0:
+                    expenses +=1
+    try:
+        return sum_expenses() /expenses
+    except ZeroDivisionError:
+        print("There are no expenses available, so average is 0")
 
 
+def categorise_expenses():
+    #scan through document and find repeated categories- but how to do
+    #that if you cannot predict the categories? Could use a counter to weed
+    # out any categories that don't appear more than once then re-read the
+    # file and total expenses in a dictionary for each category (using for
+    # loop and split function
+    return
 
-#basic summary - shows total spent and how much budget left for the month
+
+#basic summary - shows total spent and how much budget left for the month✅
 #customs - add percentages on what expenses they were spent on ? average
-# spent per expense? median amount spent? average of each category?
+# spent per expense✅? median amount spent? average of each category?
 
